bfb3cbe1e4966c5c214dbca0e09884ddf1b189cf
Ryan Marchock <ryan@bynd.com>
2014-05-26 21:10:57 +0100

    Fixed - creating tiles in the correct position

--- js/jquery.jigsaw.js
+++ js/jquery.jigsaw.js
@@ -90,32 +90,54 @@
 
                 demoBlocks: function () {
                     return [
-                        {x: 1, y: 1},
                         {x: 2, y: 2},
-                        {x: 1, y: 1},
-                        {x: 2, y: 1},
-                        {x: 1, y: 1},
-                        {x: 2, y: 1},
-                        {x: 2, y: 1},
                         {x: 2, y: 2},
-                        {x: 1, y: 1},
-                        {x: 1, y: 1},
-                        {x: 2, y: 1},
+                        {x: 1, y: 2},
                         {x: 2, y: 1},
                         {x: 1, y: 1},
-                        {x: 2, y: 1},
-                        {x: 2, y: 1},
+                        {x: 1, y: 1},
+                        {x: 1, y: 2},
+                        {x: 2, y: 2},
+                        {x: 1, y: 2},
+                        {x: 2, y: 2},
+                        {x: 1, y: 2},
                         {x: 1, y: 2},
                         {x: 2, y: 1},
                         {x: 2, y: 1},
                         {x: 1, y: 2},
-                        {x: 1, y: 1}
+                        {x: 1, y: 1},
+                        {x: 2, y: 2},
+                        {x: 2, y: 2},
+                        {x: 2, y: 1},
+                        {x: 2, y: 1},
+                        {x: 2, y: 2},
+                        {x: 2, y: 2},
+                        {x: 1, y: 2},
+                        {x: 2, y: 1},
+                        {x: 1, y: 1},
+                        {x: 1, y: 1},
+                        {x: 1, y: 2},
+                        {x: 2, y: 2},
+                        {x: 1, y: 2},
+                        {x: 2, y: 2},
+                        {x: 1, y: 2},
+                        {x: 1, y: 2},
+                        {x: 2, y: 1},
+                        {x: 2, y: 1},
+                        {x: 1, y: 2},
+                        {x: 1, y: 1},
+                        {x: 2, y: 2},
+                        {x: 2, y: 2},
+                        {x: 2, y: 1},
+                        {x: 2, y: 1}
                     ]
                 },
 
 
                 browserResized: function (containerWidth) {
                     this.containerWidth = containerWidth;
+
+                    this.startLoop = 0;
 
                     // calculate the number of columns 
                     this.cols = Math.round(this.containerWidth / this.settings.tileWidth);
@@ -130,55 +152,76 @@
                     // get end of file number
                     this.eof += 20;
 
-                    this.startLoop += 20;
+                    // start position of loop
+                    this.startLoop = this.tiles.length;
 
-                   this.solveJigsaw(); 
+                    this.solveJigsaw(); 
                 },
 
                 solveJigsaw: function (containerWidth) {
 
-                    
-                    // get the number of tile rows
-                    this.rows = 0;
-
-
                     var testTemplate = this.demoBlocks(),
                         i = 0,
+                        eof = this.eof,
                         x,  
                         y;
+                    
 
-                    // created array with tile properties and find total number of tiles
+                    if (this.tiles.length < eof) {
 
+                        for (i = this.startLoop; i < eof; i += 1) {
 
+                            if (this.settings.demo) {
+                                x = testTemplate[i].x;
+                                y = testTemplate[i].y;
+                            } else {
+                                x = this.getNum();
+                                y = this.getNum();
+                            }
 
-                    // TODO - if browser is resized reload tiles into position not create new tiles 
+                            // apply tile template to tiles array
+                            this.tiles[i] = this.tileTemplate(x, y);
 
+                            // calculate the total number of tiles
+                            this.numOfTiles += (x * y)
 
-
-                    for (i = this.startLoop; i < this.eof; i += 1) {
-
-                        if (this.settings.demo) {
-                            x = testTemplate[i].x;
-                            y = testTemplate[i].y;
-                        } else {
-                            x = this.getNum();
-                            y = this.getNum();
                         }
 
-                        // apply tile template to tiles array
-                        this.tiles[i] = this.tileTemplate(x, y);
+                    } else {
 
-                        // calculate the total number of tiles
-                        this.numOfTiles += (x * y)
 
-                        //console.log(i, this.tiles, this.numOfTiles)
+                        // reset all tiles created to false when browser resize 
+
+                        //
+                        for (i = 0; i < this.tiles.length; i += 1) {
+                            this.tiles[i].created = false;
+                        }
+                        this.grid = [];
+
                     }
+
+
+
+
+
 
                     this.rows = Math.round(this.numOfTiles / this.cols);
 
-                    for (i = this.grid.length; i <= this.rows+1; i += 1) {
-                        this.grid[i] = this.setArrayLength(this.cols)
+                    
+
+                    for (i = 0; i < this.rows; i += 1) {
+
+                        // if grid row has no array then create column
+                        if (!this.grid[i]) {
+                            this.grid[i] = this.setArrayLength(this.cols)
+
+                            //console.log("CREATE ROW", i)
+                        }
+                        
                     }
+
+
+                    //console.log("solveJigsaw", this.rows, this.grid, this.numOfTiles, this.cols)
 
                     this.build();
                 },
@@ -188,12 +231,14 @@
                     var i = 0,
                         acC = 0,
                         acR = 0,
-                        tc = 0,
+                        tc = this.startLoop,
                         addTile;
                     /*
                      * Set-up grid array
                      */
                     for (i=0; i < this.numOfTiles; i += 1) {
+
+                        //this.consoleLogGrid(this.grid);
                         
                         addTile = true;
 
@@ -201,6 +246,9 @@
 
                         // acR = array counter row
                         // acC = array counter column 
+
+
+                        //console.log("for loop", this.grid[acR][acC], i, tc)
 
                         if (!this.grid[acR][acC]) {
 
@@ -211,7 +259,7 @@
                              */
                             if (this.isTileCreated(tc, 2, 2)) {
 
-                                if (this.gridHasSpace(acR, acC)) {
+                                if (this.gridHasSpace(this.tiles[tc].className, acR, acC)) {
 
                                     this.updateGrid(this.tiles[tc].className, acR, acC); 
 
@@ -229,7 +277,7 @@
                             } else if (this.isTileCreated(tc, 2, 1)) {
 
 
-                                if (this.gridHasSpace(acR, acC)) {
+                                if (this.gridHasSpace(this.tiles[tc].className, acR, acC)) {
 
                                     this.updateGrid(this.tiles[tc].className, acR, acC); 
 
@@ -246,7 +294,14 @@
                              */
                             } else if (this.isTileCreated(tc, 1, 2)) {
 
-                                this.updateGrid(this.tiles[tc].className, acR, acC);
+
+
+                                if (this.gridHasSpace(this.tiles[tc].className, acR, acC)) {
+                                    this.updateGrid(this.tiles[tc].className, acR, acC);
+                                } else {
+                                    addTile = false;
+                                    this.searchForTile(tc, acR, acC);
+                                }
 
 
 
@@ -284,7 +339,8 @@
                         /**
                          * Update array counters 
                          *
-                         * 
+                         * tracks the position of the grid by row and column 
+                         * starts from left to right and then down left to start again
                          */
                         if (acC < this.cols-1) {
                             acC += 1;
@@ -297,32 +353,70 @@
 
                         if (!this.tiles[tc] || tc === this.stopPoint) {
                             console.log("COMPLETED", acR)
+
+                            //this.consoleLogGrid(this.grid);
+                            //console.log(this.tiles);
                             break;
+                        }
+
+
+                        if (tc < this.stopPoint && i >= (this.numOfTiles - 1)) {
+
+                            console.log("BROKEN", this.grid.length, acR)
+
+                            if (this.grid.length === acR) {
+                                this.grid[this.grid.length] = this.setArrayLength(this.cols)
+                            }
+
+                            this.numOfTiles++;
                         }
                     }
 
+
+                    //console.log(this.tiles);
+                    //this.consoleLogGrid(this.grid);
+
                     // Apply height to container
-                    $(this.element)[0].style.height = ((acR + 1) * this.settings.tileWidth) + "px";
+
+                    // TODO: check the last row inside of grid and see if any tiles have 
+                    // been applied. to Fix container height 
+
+                    // delete rows that are empty 
+
+                    $(this.element)[0].style.height = (this.grid.length * this.settings.tileWidth) + "px";
                 },
 
 
                 searchForTile: function (index, acR, acC) {
+                    // search for a tile to fit inside grid position row and column
                     var c = index;
 
                     for (c = c; c < this.tiles.length; c += 1) {
 
                         if (this.tiles[c].w === 1 && !this.tiles[c].created) {
 
-                            this.createTile(this.tiles[c].className, acC, acR, this.tiles[c], c);
-                            this.updateGrid(this.tiles[c].className, acR, acC);
-                            this.tiles[c].created = true;
-                            this.updateTile(index, acR, acC);
+                            // check grid space available goes here 
 
-                            break;
+                            // creating a new 20 will sometimes cause duplicates and tiles positioning
+                            // on top of another
+
+                            //console.log("FIND", this.tiles[c].className, acR, acC, c, this.gridHasSpace(this.tiles[c].className, acR, acC))
+
+                            if (this.gridHasSpace(this.tiles[c].className, acR, acC)) {
+
+                                this.createTile(this.tiles[c].className, acC, acR, this.tiles[c], c);
+                                this.updateGrid(this.tiles[c].className, acR, acC);
+                                this.tiles[c].created = true;
+                                this.updateTile(index, acR, acC);
+
+                                break;
+                            }
 
                         } else if (this.tiles[c].w === 2 && !this.tiles[c].created) {
 
-                            if (this.gridHasSpace(acR, acC)) {
+                            //console.log("FIND", this.tiles[c].className, acR, acC, c)
+
+                            if (this.gridHasSpace(this.tiles[c].className, acR, acC)) {
 
                                 this.createTile(this.tiles[c].className, acC, acR, this.tiles[c], c);
                                 this.updateGrid(this.tiles[c].className, acR, acC);
@@ -357,14 +451,86 @@
                 },
 
 
-                gridHasSpace: function (row, column) {
+                // gridHasSpace: function (row, column) {
+
+
+                //     // check for 1 X 2 and 2 X 2
+
+                //     // use class name to identify which box to check
+
+
+                //     var b = false;
+
+                //     if (!this.grid[row][column + 1] && 
+                //         (column + 1) < this.cols) {
+                //         b = true;
+                //     }
+                //     return b;
+                // },
+
+                gridHasSpace: function (string, row, column) {
                     var b = false;
 
-                    if (!this.grid[row][column + 1] && 
-                        (column + 1) < this.cols) {
-                        b = true;
+                    switch(string) {
+                        case "box-1-1":
+
+                            // 1 0
+                            // 0 0
+
+                            b = true;
+                          
+                          break;
+                        case "box-2-1":
+
+                            // 1 1
+                            // 0 0
+
+                            if (!this.grid[row][column + 1] &&
+                                (column + 1) < this.cols) {
+
+                                b = true;
+                            }
+                          
+                          break;
+                        case "box-1-2":
+
+                            // 1 0
+                            // 1 0
+
+                            if (this.grid.length === (row + 1)) {
+                                console.log("EXTEND ARRAY ROW")
+                                this.grid[this.grid.length] = this.setArrayLength(this.cols)
+                            }
+
+                            if (!this.grid[row + 1][column]) {
+                                //console.log("grid has space 2x1", this.grid[row + 1][column])
+                                b = true;
+                            }
+                          
+                          break;
+                        case "box-2-2":
+
+                            // 1 1
+                            // 1 1
+
+                            if (this.grid.length === (row + 1)) {
+                                console.log("EXTEND ARRAY ROW")
+                                this.grid[this.grid.length] = this.setArrayLength(this.cols)
+                            }
+
+                            if (!this.grid[row][column + 1] &&
+                                !this.grid[row + 1][column] &&
+                                !this.grid[row + 1][column + 1] &&
+                                (column + 1) < this.cols) {
+
+                                b = true;
+                            }
+                          break;
                     }
-                    return b;
+
+                    //console.log(string, b)
+
+                        return b;
                 },
 
                 updateGrid: function (string, row, column) {
